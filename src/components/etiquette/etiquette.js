19dfe1ca883d251efa6a8c270f225e75f42206ee
min830-a <llhmyll@naver.com>
2025-07-24 16:07:15 +0900

    feat : 마크업 수정과 css스타일 코드 - 마크업 수정 button 태그 위치 수정 - css 스타일 작성 - js 변수 설정

---
+++ src/components/etiquette/etiquette.js
@@ -0,0 +1,10 @@
+const etiquette = document.querySelector('.etiquette')
+// const countryLabel = etiquette.querySelector('.etiquette-label')
+
+const cardContainer = document.querySelector('.etiquette-cards')
+const cards = cardContainer.querySelectorAll('.etiquette-card')
+
+const prevButton = etiquette.querySelector('.etiquette-nav.prev')
+const nextButton = etiquette.querySelector('.etiquette-nav.next')
+
+
