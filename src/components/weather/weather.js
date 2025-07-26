583d19183f31d7e090a5102fe489201664b90a8f
Heosion <tldhs123e@gmail.com>
2025-07-27 00:27:58 +0900

    feat : 날씨 API 컴포넌트(html, css, js) 구현

---
+++ src/components/weather/weather.js
@@ -0,0 +1,98 @@
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+// 날씨 이미지 받아오기
+function getWeatherImage(weatherMain) {
+  const weatherIconMap = {
+    Clear: "sunny.svg",
+    Clouds: "cloudy.svg",
+    Rain: "rainy-01.svg",
+    Thunderstorm: "rainy-02.svg",
+    Drizzle: "rainy-03.svg",
+    Snow: "snow.svg",
+  };
+  return `../../../src/assets/svg/${weatherIconMap[weatherMain] || "default.svg"}`;
+}
+
+function getWeather(lat, lon) {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const currentDate = new Date(data.list[0].dt_txt);
+      const currentDay = currentDate.toLocaleDateString("en-US", {
+        weekday: "short",
+      });
+
+      // 현재 도시와 온도 정보
+      const city = data.city.name;
+      const currentTemp = Math.round(data.list[0].main.temp);
+      document.querySelector(".city").textContent = city;
+      document.querySelector(".temp").textContent = `${currentTemp}°`;
+
+      // 현재 날씨 정보
+      const currentWeatherMain = data.list[0].weather[0].main;
+      const currentWeatherDesc = data.list[0].weather[0].description;
+      const currentImgURL = getWeatherImage(currentWeatherMain);
+
+      const dailyForecast = {};
+      data.list.forEach((item) => {
+        const date = new Date(item.dt_txt);
+        const day = date.toLocaleDateString("en-US", { weekday: "short" });
+        if (!dailyForecast[day]) {
+          dailyForecast[day] = item;
+        }
+      });
+
+      // 현재 날씨 아이콘 출력
+      const forecastEl = document.querySelector(".forecast");
+      forecastEl.innerHTML = `
+			<div class="current-weather-image">
+			<span class="current-day-label">
+			<span>${currentDay.toUpperCase()}</span>
+          <img src="${currentImgURL}" alt="${currentWeatherDesc}">
+        </div>
+      `;
+
+      // 날씨 다음 2~3일날 받아오기
+      Object.entries(dailyForecast)
+        .slice(1, 3)
+        .forEach(([day, info]) => {
+          const weatherMain = info.weather[0].main;
+          const imgURL = getWeatherImage(weatherMain);
+          const weatherDesc = info.weather[0].description;
+          forecastEl.innerHTML += `
+            <div class="forecast-day">
+              <div>${day.toUpperCase()}</div>
+              <img src="${imgURL}" alt="${weatherDesc}" style="width:36px; height:36px;">
+            </div>
+          `;
+        });
+    })
+    .catch((err) => {
+      document.querySelector(".city").textContent =
+        "날씨 정보를 가져올 수 없습니다.";
+      console.error(err);
+    });
+}
+
+function getUserLocation() {
+  if (!navigator.geolocation) {
+    alert("현재 브라우저는 위치 정보를 지원하지 않습니다.");
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      const { latitude, longitude } = pos.coords;
+      getWeather(latitude, longitude);
+    },
+    (err) => {
+      alert("위치 정보를 가져올 수 없습니다.");
+      console.error(err);
+    }
+  );
+}
+
+// 실행
+getUserLocation();
