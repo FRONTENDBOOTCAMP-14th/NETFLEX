a3afc8348eb64d8055099c9570e600325754c9c2
wjinss <aastu4343@gmail.com>
2025-07-30 17:19:05 +0900

    feat : 날씨 컴포넌트 호출 및 index.html 배치

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,37 +1,38 @@
+import './weather.css';
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 // 날씨 이미지 받아오기
 function getWeatherImage(weatherMain) {
   const weatherIconMap = {
-    Clear: "sunny.svg",
-    Clouds: "cloudy.svg",
-    ScatteredClouds: "cloudy-01.svg",
-    FewClouds: "cloudy-03.svg",
-    Rain: "rainy-01.svg",
-		ThunderstormWithRain: "rainy-02.svg",
-		DrizzleRain: "rainy-03.svg",
-    Snow: "snow.svg",
-    Thunderstorm: "thunder.svg",
+    Clear: 'sunny.svg',
+    Clouds: 'cloudy.svg',
+    ScatteredClouds: 'cloudy-01.svg',
+    FewClouds: 'cloudy-03.svg',
+    Rain: 'rainy-01.svg',
+    ThunderstormWithRain: 'rainy-02.svg',
+    DrizzleRain: 'rainy-03.svg',
+    Snow: 'snow.svg',
+    Thunderstorm: 'thunder.svg',
   };
-  return `../../../src/assets/svg/${weatherIconMap[weatherMain] || "default.svg"}`;
+  return `../../../src/assets/svg/${weatherIconMap[weatherMain] || 'default.svg'}`;
 }
 
 function getWeather(lat, lon) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
   fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
+    .then(res => res.json())
+    .then(data => {
       const currentDate = new Date(data.list[0].dt_txt);
-      const currentDay = currentDate.toLocaleDateString("en-US", {
-        weekday: "short",
+      const currentDay = currentDate.toLocaleDateString('en-US', {
+        weekday: 'short',
       });
 
       // 현재 도시와 온도 정보
       const city = data.city.name;
       const currentTemp = Math.round(data.list[0].main.temp);
-      document.querySelector(".city").textContent = city;
-      document.querySelector(".temp").textContent = `${currentTemp}°`;
+      document.querySelector('.city').textContent = city;
+      document.querySelector('.temp').textContent = `${currentTemp}°`;
 
       // 현재 날씨 정보
       const currentWeatherMain = data.list[0].weather[0].main;
@@ -39,16 +40,16 @@ function getWeather(lat, lon) {
       const currentImgURL = getWeatherImage(currentWeatherMain);
 
       const dailyForecast = {};
-      data.list.forEach((item) => {
+      data.list.forEach(item => {
         const date = new Date(item.dt_txt);
-        const day = date.toLocaleDateString("en-US", { weekday: "short" });
+        const day = date.toLocaleDateString('en-US', { weekday: 'short' });
         if (!dailyForecast[day]) {
           dailyForecast[day] = item;
         }
       });
 
       // 현재 날씨 아이콘 출력
-      const forecastEl = document.querySelector(".forecast");
+      const forecastEl = document.querySelector('.forecast');
       forecastEl.innerHTML = `
 			<div class="current-weather-image">
 			<span class="current-day-label">
@@ -72,28 +73,28 @@ function getWeather(lat, lon) {
           `;
         });
     })
-    .catch((err) => {
-      document.querySelector(".city").textContent =
-        "날씨 정보를 가져올 수 없습니다.";
+    .catch(err => {
+      document.querySelector('.city').textContent =
+        '날씨 정보를 가져올 수 없습니다.';
       console.error(err);
     });
 }
 
 function getUserLocation() {
   if (!navigator.geolocation) {
-    alert("현재 브라우저는 위치 정보를 지원하지 않습니다.");
+    alert('현재 브라우저는 위치 정보를 지원하지 않습니다.');
     return;
   }
 
   navigator.geolocation.getCurrentPosition(
-    (pos) => {
+    pos => {
       const { latitude, longitude } = pos.coords;
       getWeather(latitude, longitude);
     },
-    (err) => {
-      alert("위치 정보를 가져올 수 없습니다.");
+    err => {
+      alert('위치 정보를 가져올 수 없습니다.');
       console.error(err);
-    }
+    },
   );
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import './utils/header_component.js';
 import '../components/navigation/navigation.js';
 import '../components/schedule/schedule.js';
+import '../components/weather/weather.js';
